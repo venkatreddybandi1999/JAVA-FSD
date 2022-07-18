f5ae08198f97a71ab911fb885dc75e5e2fe8dd2c
Venkat reddy <103410334+venkatreddybandi1999@users.noreply.github.com>
2022-07-18 14:01:43 +0530

    Calculator Java Program

---
+++ Calculator.java
@@ -0,0 +1,51 @@
+package com.simpliiearn.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+	
+	public static void main (String [] args) {
+		
+		//store two numbers
+		double num1,num2,res;
+		
+		//take input from the user
+		Scanner sc = new Scanner (System.in);
+		
+		
+		System.out.println("Enter the numbers");
+		
+		//take inputs from user
+		num1 =sc.nextDouble();
+		num2 =sc.nextDouble();
+		
+		//choosing the operators
+		System.out.println("choose the opeartors(+,-,*,/)");
+		
+		char operator=sc.next().charAt(0);
+		double output=0.0;
+		
+		switch (operator) {
+		
+		case '+' :
+			output = num1+num2;
+			System.out.println(output);
+			break;
+			
+		case '-' :
+			output = num1-num2;
+			System.out.println(output);
+			break;
+			
+		case '*' :
+			output = num1*num2;
+			System.out.println(output);
+			break;
+			
+		case '/' :
+			output = num1/num2;
+			System.out.println(output);
+			break;
+		}	
+	}
+}
